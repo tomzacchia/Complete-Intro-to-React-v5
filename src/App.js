baf4fc12df7fa2877d84634e5928695263a50738
tomzacchia <tomzacchia@live.com>
2020-11-08 20:27:43 -0500

    Convert App to jsx

--- src/App.js
+++ src/App.js
@@ -3,25 +3,15 @@ import { render } from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  // React.createElement( type, [props],[...children])
-  return React.createElement("div", { id: "unique_id" }, [
-    React.createElement("h1", {}, "Adopt Me"),
-    React.createElement(Pet, {
-      name: "CeeCee",
-      animal: "Dog",
-      breed: "Black Cur",
-    }),
-    React.createElement(Pet, {
-      name: "Deegan",
-      animal: "Dog",
-      breed: "Black Lab",
-    }),
-    React.createElement(Pet, {
-      name: "Peaches",
-      animal: "Cat",
-      breed: "Funny Cat",
-    }),
-  ]);
+  return (
+    <div>
+      <h1 id="something-important"> Adopt Me! </h1>
+
+      <Pet name="Luna" animal="Dog" breed="Black Cur" />
+      <Pet name="Deegan" animal="Dog" breed="Black Lab" />
+      <Pet name="Peaches" animal="Cat" breed="Some Cat" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.getElementById("root"));
+render(<App />, document.getElementById("root"));
