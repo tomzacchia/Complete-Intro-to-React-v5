cd3df7d3fe5648f66fc2cdc6320112427ca874b1
tomzacchia <tomzacchia@live.com>
2020-11-11 17:07:11 -0500

    Remove comments from 4-hooks merge

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,4 +1,3 @@
-// useEffect takes place of: componentDidMOunt, ...wilMount, ...wilUpdate
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
 import useDropdown from "./useDropdown";
@@ -9,29 +8,20 @@ const SearchParams = () => {
   const [animal, AnimalDropdown] = useDropdown("Animals", "dog", ANIMALS);
   const [breed, BreedDropdown, setBreed] = useDropdown("Breed", "", breeds);
 
-  // schedules function call to happen after component renders
-  // the 2nd argument to useEffect is a list of dependencies
-  // which it uses to determine if the callback should be called
-  // this effect does not depend on breeds
   useEffect(() => {
     setBreeds([]);
     setBreed("");
 
-    // call API
     pet.breeds(animal).then(({ breeds: apiBreeds }) => {
       var breedStringsArr = apiBreeds.map((breedObj) => breedObj.name);
 
       setBreeds(breedStringsArr);
     }, console.error);
-    // same as error => console.error(error)
   }, [animal, setBreed, setBreeds]);
-  // runs once pass in no dependencies
 
-  // All of this renders first
   return (
     <div className="search-params">
       <form>
-        {/* input inside label is good for accessibility */}
         <label htmlFor="location">
           Location
           <input
@@ -39,7 +29,6 @@ const SearchParams = () => {
             id="location"
             value={location}
             placeholder="Location"
-            // typing triggers a re-render of SearchParams
             onChange={(event) => setLocation(event.target.value)}
           />
         </label>
