71dee5b97360f90b3837cbfd0998c6af9cc4a945
tomzacchia <tomzacchia@live.com>
2020-11-12 16:01:47 -0500

    Remove comments from merge 5-async-and-routing

--- src/App.js
+++ src/App.js
@@ -8,17 +8,9 @@ const App = () => {
   return (
     <div>
       <header>
-        {/* 
-          compiled to anchor tag, together Link will 
-          handle routing mechanisms 
-        */}
         <Link to="/">Adopt Me!</Link>
       </header>
       <Router>
-        {/* 
-          - Reach router only renders what matches "the most" 
-          - Order declaration of routes does not matter
-        */}
         <SearchParams path="/" />
         <Details path="/details/:id" />
       </Router>
