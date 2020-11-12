747739459bdd0b30d8958c33565dbbe1a62379c0
tomzacchia <tomzacchia@live.com>
2020-11-12 15:15:18 -0500

    Refactor Pet component

--- src/Pet.js
+++ src/Pet.js
@@ -1,14 +1,21 @@
 import React from "react";
 
-// Parcel comes with babel
+export default function Pet({ name, animal, breed, media, location, id }) {
+  let hero = "http://placecorgi.com/300/300";
 
-export default function Pet({ name, animal, breed }) {
+  if (media.length) {
+    hero = media[0].small;
+  }
+
   return (
-    // div gets transpiled to React.createElement so we need to import React
-    <div>
-      <h1> {name} </h1>
-      <h2> {animal} </h2>
-      <h2> {breed} </h2>
-    </div>
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{`${animal} - ${breed} - ${location}`}</h2>
+      </div>
+    </a>
   );
 }
