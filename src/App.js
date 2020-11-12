b132f34a614e535929cbcad211cd3a3c54b44493
tomzacchia <tomzacchia@live.com>
2020-11-12 15:53:47 -0500

    Add reach router to App component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { render } from "react-dom";
 import SearchParams from "./SearchParams";
+import { Router } from "@reach/router";
+import Details from "./Details";
 
 const App = () => {
   return (
     <div>
       <h1 id="something-important"> Adopt Me! </h1>
-
-      <SearchParams />
+      <Router>
+        {/* 
+          - Reach router only renders what matches "the most" 
+          - Order declaration of routes does not matter
+        */}
+        <SearchParams path="/" />
+        <Details path="/details/:id" />
+      </Router>
     </div>
   );
 };
