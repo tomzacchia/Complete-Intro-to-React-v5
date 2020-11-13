c6b3d24ce6bcfa1f79b5287c88b4946bea1337ad
tomzacchia <tomzacchia@live.com>
2020-11-13 16:00:34 -0500

    Refactor details component from function to class

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,36 @@
 import React from "react";
+import { default as PetAPI } from "@frontendmasters/pet";
 
-const Details = (props) => {
-  return <h1> Hi </h1>;
-};
+class Details extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: true,
+    };
+  }
+
+  // only runs once during component creation
+  componentDidMount() {
+    // we have to use arrow functions in this case because
+    // normal functions are envoked by something that is not Details
+    PetAPI.animal(this.props.id).then(({ animal }) => {
+      // this.setState is a shallow merge deep levels are overwritten
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        media: animal.photos,
+        breed: animal.breeds.primary,
+        loading: false,
+      });
+    }, console.error);
+  }
+
+  render() {
+    return <h1> Hi </h1>;
+  }
+}
 
 export default Details;
