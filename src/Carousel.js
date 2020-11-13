6b30f681d6703f6d6cdbae1de57783a2b9e63786
tomzacchia <tomzacchia@live.com>
2020-11-13 18:07:34 -0500

    Add event handler for img onClick

---
+++ src/Carousel.js
@@ -0,0 +1,52 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
+import React from "react";
+
+class Carousel extends React.Component {
+  state = {
+    photos: [],
+    activeIndex: 0,
+  };
+
+  // we can perform transformations on incoming props
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600/600"];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    // return object is merged with state
+    return { photos };
+  }
+
+  // the click event trigger handleIndexClick as a callback
+  // if handlers are not arrow functions we lost this context
+  handleIndexClick = (event) => {
+    this.setState({ activeIndex: event.target.dataset.index });
+  };
+
+  render() {
+    const { photos, activeIndex } = this.state;
+
+    return (
+      <div className="carousel">
+        <img src={photos[activeIndex]} alt="animal" />
+        <div className="carousel-smaller">
+          {photos.map((photo, index) => {
+            <img
+              key={photo}
+              src={photo}
+              onClick={this.handleIndexClick}
+              data-index={index}
+              className={index === activeIndex ? "active" : ""}
+              alt="animal thumbnail"
+            />;
+          })}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Carousel;
